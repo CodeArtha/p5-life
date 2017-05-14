fbc621cbb274f72c615cfe9e81694e8bc4a63105
CodeArtha <codeartha@gmail.com>
2017-05-14 23:47:10 +0200

    Basic game board, 2D array

--- cell.js
+++ cell.js
@@ -1,15 +1,52 @@
-function Cell(){
+/**
+Cell state:
+0 = dead (black)
+1 = live (white)
 
+Cell nextState:
+0 = going to die (black)
+1 = born (white)
+*/
+
+function Cell(c, r, s){
+	this.col = c;
+	this.row = r;
+	this.x = c*scl;
+	this.y = r*scl;
+	this.state = s;
+	this.nextState;
 
 	this.show = function(){
-
+		if(this.state == 0){
+			fill(0,0,0);
+			rect(this.x, this.y, scl, scl);
+		} else if( this.state == 1) {
+			fill(255, 255, 255);
+			rect(this.x, this.y, scl, scl);
+		}
 	}
 
 	this.update = function(){
-
+		if(this.col == 0 || this.col == cols - 1 || this.row == 0 || this.row == rows - 1){
+			this.state = 0;
+			this.nextState = 0;
+		}else{
+			n = this.countNeighbors();
+		}
 	}
 
 	this.countNeighbors = function(){
-		
+		var count = 0;
+
+		if(grid[this.col - 1][this.row -1 ].state != 0) count++;
+		if(grid[this.col][this.row - 1].state != 0) count++;
+		if(grid[this.col + 1][this.row - 1].state != 0) count++;
+		if(grid[this.col - 1][this.row].state != 0) count++;
+		if(grid[this.col + 1][this.row].state != 0) count++;
+		if(grid[this.col - 1][this.row + 1].state != 0) count++;
+		if(grid[this.col][this.row + 1].state != 0) count++;
+		if(grid[this.col + 1][this.row + 1].state != 0) count++;
+
+		return count;
 	}
 }
--- sketch.js
+++ sketch.js
@@ -1,7 +1,41 @@
-function setup() {
+var cols = 20;
+var rows = 15;
+var scl = 20;
+var grid = [];
 
+function setup() {
+	createCanvas(cols * scl, rows * scl);
+	frameRate(1);
+
+	initGrid();
+	randomGrid();
 }
 
 function draw() {
+	background(50);
+	
+	for(var r = 0; r < rows; r++){
+		for(var c = 0; c < cols; c++){
+			grid[c][r].update();
+			grid[c][r].show();
+		}
+	}
+}
 
+function initGrid(){
+	for(var c = 0; c < cols; c++){
+		grid[c] = [];
+	}
+}
+
+function randomGrid(density = 0.5){
+	for(var r = 0; r < rows; r++){
+		for(var c = 0; c < cols; c++){
+			if(random() <= density){
+				grid[c][r] = new Cell(c, r, 1);
+			}else{
+				grid[c][r] = new Cell(c, r, 0);
+			}
+		}
+	}
 }
